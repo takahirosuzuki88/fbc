777271820cd5cb9aed1542be8995a99eb2a1cd81
takahiro suzuki <tery800@hotmail.com>
2017-02-26 12:05:07 +0000

    create sample17.md

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,11 @@
 100.times do |n|
   email = Faker::Internet.email
   password = "password"
+  name = "name"
   User.create!(email: email,
                password: password,
                password_confirmation: password,
+               name: name,
                )
 end
 
