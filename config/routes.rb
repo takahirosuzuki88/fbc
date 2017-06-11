7c1eacb861c1a5146702be87f976ed22e37e765b
takahiro suzuki <tery800@hotmail.com>
2017-06-11 07:23:42 +0000

    フォローページ作成3

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ Rails.application.routes.draw do
 
  resources :relationships, only: [:create, :destroy]
 
+ resources :conversations do
+  resources :messages
+ end
+
  devise_for :users, controllers: {
    registrations: "users/registrations",
    omniauth_callbacks: "users/omniauth_callbacks"
